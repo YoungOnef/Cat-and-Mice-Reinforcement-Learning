c5f5aa621691025d100bea34335a7df4c473d186
YoungOne <72264732+YoungOnef@users.noreply.github.com>
2023-03-21 15:43:38 +0000

    Main AI trained, working on second Target AI

---
+++ Assets/Scripts/FleeFromAgent.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using static UnityEngine.GraphicsBuffer;
+
+public class FleeFromAgent : Agent
+{
+    [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float timeSurvivedReward = 0.001f;
+    private float timer;
+    private MoveToGoalAgent moveToGoalAgent;
+    public override void OnEpisodeBegin()
+    {
+        timer = 0f;
+        //moveToGoalAgent = GetComponent<MoveToGoalAgent>();
+        moveToGoalAgent = GameObject.Find("Agent").GetComponent<MoveToGoalAgent>();
+    }
+
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float rotateAmount = actions.ContinuousActions[0];
+        float moveAmount = actions.ContinuousActions[1];
+
+
+        transform.Rotate(Vector3.up, rotateAmount * Time.deltaTime * rotationSpeed);
+
+        transform.localPosition += transform.forward * moveAmount * Time.deltaTime * moveSpeed;
+
+        // Add reward for time survived
+        timer += Time.deltaTime;
+        AddReward(timeSurvivedReward * timer);
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Horizontal");
+        continuousActions[1] = Input.GetAxis("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Agent"))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+
+
+        if (other.CompareTag("Wall"))
+        {
+            print("Flee from agent Wall collided");
+            SetReward(-1f);
+            EndEpisode();
+            moveToGoalAgent.OnEpisodeBegin();
+
+
+        }
+    }
+}
--- Assets/Scripts/MoveToGoalAgent.cs
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using System;
 
 public class MoveToGoalAgent : Agent
 {
     [SerializeField] private GameObject targetPrefab;
-    [SerializeField] private int numTargets = 20;
-    [SerializeField] private float timePenalty = -0.01f;
-    
+    //[SerializeField] 
+    private int numTargets = 1;
+    [SerializeField] private float timePenalty = -0.0001f;
+
     [SerializeField] private float rotationSpeed = 200f;
     [SerializeField] private float moveSpeed = 5;
     private List<Transform> targets;
@@ -35,11 +37,14 @@ public class MoveToGoalAgent : Agent
         // Spawn new targets
         for (int i = 0; i < numTargets; i++)
         {
-            Vector3 targetPosition = environmentTransform.TransformPoint(new Vector3(Random.Range(-19f, 19f), 0, Random.Range(-13f, 13f)));
+            Vector3 targetPosition = environmentTransform.TransformPoint(new Vector3(UnityEngine.Random.Range(-19f, 19f), 0, UnityEngine.Random.Range(-13f, 13f)));
+
 
             GameObject targetInstance = Instantiate(targetPrefab, targetPosition, Quaternion.identity);
             targets.Add(targetInstance.transform);
         }
+
+
 
         transform.localPosition = new Vector3(0, 0, 0);
         transform.rotation = Quaternion.identity;
@@ -49,16 +54,9 @@ public class MoveToGoalAgent : Agent
     {
         float rotateAmount = actions.ContinuousActions[0];
         float moveAmount = actions.ContinuousActions[1];
-
-
-        // Rotate the agent
         transform.Rotate(Vector3.up, rotateAmount * Time.deltaTime * rotationSpeed);
-
-        // Move the agent forward or backward
         transform.localPosition += transform.forward * moveAmount * Time.deltaTime * moveSpeed;
 
-        SetReward(timePenalty * (1 - Mathf.Abs(rotateAmount) - Mathf.Abs(moveAmount)));
-        
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -72,13 +70,12 @@ public class MoveToGoalAgent : Agent
     {
         if (other.CompareTag("Goal"))
         {
-            SetReward(+1.0f);
+            SetReward(+0.1f);
             targets.Remove(other.transform);
             Destroy(other.gameObject);
-
-            // End episode if all targets are collected
             if (targets.Count == 0)
             {
+                print("Targets collected");
                 float timeRemaining = (MaxStep - StepCount) / (float)MaxStep;
                 SetReward(1f + timeRemaining);
                 EndEpisode();
@@ -87,8 +84,9 @@ public class MoveToGoalAgent : Agent
 
         if (other.CompareTag("Wall"))
         {
-            SetReward(-10f);
+            SetReward(-1f);
             EndEpisode();
         }
     }
+
 }
