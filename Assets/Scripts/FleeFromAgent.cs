c620193a569039d75fc8e64b158618a2f5638aba
YoungOne <72264732+YoungOnef@users.noreply.github.com>
2023-04-05 21:09:27 +0100

    Comments added
    Comments added

--- Assets/Scripts/FleeFromAgent.cs
+++ Assets/Scripts/FleeFromAgent.cs
@@ -8,29 +8,36 @@ using static UnityEngine.GraphicsBuffer;
 
 public class FleeFromAgent : Agent
 {
+    // Serialized fields that can be set in the Unity Editor
     [SerializeField] private float rotationSpeed = 200f;
     [SerializeField] private float moveSpeed = 5;
     [SerializeField] private float timeSurvivedReward = 0.001f;
     private float timer;
+    //
     private MoveToGoalAgent moveToGoalAgent;
+    //Restarting the timer
+
     public override void OnEpisodeBegin()
     {
         timer = 0f;
 
     }
+    //function to set the MoveToGoalAgent
     public void SetMoveToGoalAgent(MoveToGoalAgent agent)
     {
         moveToGoalAgent = agent;
     }
+    // function called when the agent receives an action from the Reinforcement Learning algorithm
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        // set the continuous actions
         float rotateAmount = actions.ContinuousActions[0];
         float moveAmount = actions.ContinuousActions[1];
 
-
+        // Rotate the agent
         transform.Rotate(Vector3.up, rotateAmount * Time.deltaTime * rotationSpeed);
-
+        // Move the agent forward
         transform.localPosition += transform.forward * moveAmount * Time.deltaTime * moveSpeed;
 
         // Add reward for time survived
@@ -39,29 +46,29 @@ public class FleeFromAgent : Agent
 
     }
 
+    // Method to manually control the agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        // Get the continuous actions
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
         continuousActions[0] = Input.GetAxis("Horizontal");
         continuousActions[1] = Input.GetAxis("Vertical");
     }
-
+    //function called when the agent collides with another object
     private void OnTriggerEnter(Collider other)
     {
+        // If the agent collides with another agent, set a negative reward and end the episode
         if (other.CompareTag("Agent"))
         {
             SetReward(-1f);
             EndEpisode();
         }
-
-
+        // If the agent collides with a wall, set a negative reward, end the episode, and reset the MoveToGoalAgent object
         if (other.CompareTag("Wall"))
         {
-            print("Flee from agent Wall collided");
             SetReward(-1f);
             EndEpisode();
             //moveToGoalAgent.OnEpisodeBegin();
-
 
         }
     }
--- Assets/Scripts/MoveToGoalAgent.cs
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -5,26 +5,30 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using System;
+using Unity.MLAgents.Integrations.Match3;
 
 public class MoveToGoalAgent : Agent
 {
+    // Serialized fields that can be set in the Unity Editor
     [SerializeField] private GameObject targetPrefab;
-    //[SerializeField] 
     private int numTargets = 10;
     [SerializeField] private float timePenalty = -0.0001f;
-
     [SerializeField] private float rotationSpeed = 200f;
     [SerializeField] private float moveSpeed = 5;
+
+    // List of targets and reference to the environment transform
     public List<Transform> targets;
     public Transform environmentTransform;
+    //function to remove a target from the list and destroy its game object
     public void RemoveTarget(Transform target)
     {
         targets.Remove(target);
         Destroy(target.gameObject);
     }
-
+    // function called at the start of each episode
     public override void OnEpisodeBegin()
     {
+        // Initialize targets list if it is null
         if (targets == null)
         {
             targets = new List<Transform>();
@@ -39,7 +43,7 @@ public class MoveToGoalAgent : Agent
             targets.Clear();
         }
 
-        // Spawn new targets
+        // Spawn new targets at random positions within the environment
         for (int i = 0; i < numTargets; i++)
         {
             Vector3 targetPosition = environmentTransform.TransformPoint(new Vector3(UnityEngine.Random.Range(-19f, 19f), 0, UnityEngine.Random.Range(-13f, 13f)));
@@ -51,37 +55,42 @@ public class MoveToGoalAgent : Agent
         }
 
 
-
+        // Reset agent position and rotation
         transform.localPosition = new Vector3(0, 0, 0);
         transform.rotation = Quaternion.identity;
     }
-
+    // function called when the agent receives an action from the Reinforcement Learning algorithm
     public override void OnActionReceived(ActionBuffers actions)
     {
+        // set the continuous actions
         float rotateAmount = actions.ContinuousActions[0];
         float moveAmount = actions.ContinuousActions[1];
+
+        // Rotate the agent
         transform.Rotate(Vector3.up, rotateAmount * Time.deltaTime * rotationSpeed);
+        // Move the agent forward
         transform.localPosition += transform.forward * moveAmount * Time.deltaTime * moveSpeed;
 
     }
-
+    // Method to manually control the agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        // Get the continuous actions
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
         continuousActions[0] = Input.GetAxis("Horizontal");
         continuousActions[1] = Input.GetAxis("Vertical");
     }
-
+    //function called when the agent collides with another object
+    // Give reward or penalty depending on whether the agent collided with a target or a wall
     private void OnTriggerEnter(Collider other)
     {
+        //If the agent collides with a target, remove the target from the list and add a reward
         if (other.CompareTag("Goal"))
         {
             SetReward(+0.1f);
             targets.Remove(other.transform);
             Destroy(other.gameObject);
-            //print Goal Destoryed left amount of obejcts
-            //print("Goal Destroyed, " + targets.Count + " targets left");
-
+            // End episode if all targets have been collected
             if (targets.Count == 0)
             {
                 print("Targets collected");
@@ -90,7 +99,7 @@ public class MoveToGoalAgent : Agent
                 EndEpisode();
             }
         }
-
+        // If the agent collides with a wall, set a negative reward, end the episode
         if (other.CompareTag("Wall"))
         {
             SetReward(-1f);
